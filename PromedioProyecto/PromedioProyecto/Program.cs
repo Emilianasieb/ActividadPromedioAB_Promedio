73fe5060414ef82108a04ff553d9cd20f4be8204
Emilianasieb <emisiebenlist00@gmail.com>
2023-06-08 20:15:01 -0300

    cambios

--- PromedioProyecto/PromedioProyecto/Program.cs
+++ PromedioProyecto/PromedioProyecto/Program.cs
@@ -1,15 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using System;
 
-namespace PromedioProyecto
+namespace ejercicioPromedio
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
+            Promedio p = new Promedio();
+            double promedio;
+            int opcionMenu;
+            do
+            {
+                Console.WriteLine("Seleccoine una opción");
+                Console.WriteLine("1. Agregar un valor");
+                Console.WriteLine("2. Calcular promedio");
+                Console.WriteLine("3. Salir");
+                opcionMenu = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+                switch (opcionMenu)
+                {
+                    case 1:
+
+                        Console.WriteLine("Ingrese un nuevo valor");
+                        p.AgregarValor(Convert.ToDouble(Console.ReadLine()));
+                        Console.Clear();
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        promedio = p.CalcularPromedio();
+                        Console.WriteLine("El promedio es: {0}", promedio);
+                        Console.ReadKey();
+                        break;
+                    default:
+                        break;
+                }
+                Console.Clear();
+            } while (opcionMenu != 3);
         }
     }
 }
